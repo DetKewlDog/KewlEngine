4652029226e0c6f445594fcb56d166aeac636d3a
Bar Goldenstein <bargolden12@gmail.com>
2023-05-15 15:24:59 +0300

    added screen & pixel (last commit was accidental)

--- KewlEngine/KewlEngine/KewlEngine/Pixel.h
+++ KewlEngine/KewlEngine/KewlEngine/Pixel.h
@@ -8,23 +8,20 @@
 #include <iostream>
 #include "Screen.h"
 
+
 class Screen;
 
 class Pixel {
 private:
 	COORD _pos = { 0, 0 };
 	Color _color, _lastColor;
-	std::thread _thread;
 	HANDLE _hConsole;
 
 public:
-	Pixel(const Pixel&) { }
 	Pixel(int x, int y) {
 		this->_pos = { (short)x, (short)y };
 		this->_hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 		this->_color = Color(255, 0, 0, 0);
-		this->_thread = std::thread(&Pixel::updatePixel, this);
-		this->_thread.detach();
 	}
 	~Pixel() { }
 
@@ -33,18 +30,11 @@ public:
 	}
 
 	void setColor(Color c) {
+		if (c == this->_color) return;
 		this->_color = c;
-	}
-
-	void updatePixel() {
-		while (true) {
-			std::this_thread::sleep_for(std::chrono::seconds(1) / 10);
-			if (this->_lastColor != this->_color) {
-				SetConsoleCursorPosition(this->_hConsole, this->_pos);
-				printf("%s", this->_color.toString().c_str());
-				this->_lastColor = this->_color;
-			}
-		}
+		SetConsoleCursorPosition(this->_hConsole, this->_pos);
+		const char* buff = this->_color.toString().c_str();
+		WriteConsoleOutputCharacter(this->_hConsole, (LPCWSTR)buff, sizeof(buff), this->_pos, nullptr);
 	}
 };
 
